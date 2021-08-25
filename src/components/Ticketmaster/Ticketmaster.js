694351f4ae3774d167b8be8cb332834dfc6147f9
Jaimes77 <JaimeRGoff@gmail.com>
2021-08-25 19:26:06 -0400

    pushing develop branch

---
+++ src/components/Ticketmaster/Ticketmaster.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Ticketmaster = (props) => {
+  return (
+    <div>
+      Hello from Ticketmaster
+      <h1>Jaime</h1>
+    </div>
+  );
+};
+
+export default Ticketmaster;
