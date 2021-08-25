a54931d514c3073e1e237d5325dd5f77ebf76b02
Jaimes77 <JaimeRGoff@gmail.com>
2021-08-25 19:46:17 -0400

    i love your face

--- src/components/nasa/Nasa.js
+++ src/components/nasa/Nasa.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Nasa = (props) => {
-  return <div>Hello from Nasa</div>;
+  return (
+    <div>
+      Hello from Nasa
+      <h1>Julia</h1>
+    </div>
+  );
 };
 
 export default Nasa;
--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,6 +1,11 @@
 import React from "react";
 const Weather = (props) => {
-  return <div>Hello from Weather</div>;
+  return (
+    <div>
+      Hello from Weather
+      <H1>Leslie</H1>
+    </div>
+  );
 };
 
 export default Weather;
