45273b82b43d6a517a36c1bc6d78ebcbd8ed4dc1
lesaphil1150 <lesaphil1150@gmail.com>
2021-08-27 21:36:38 -0400

    Leslie's Commit

--- src/components/ticketmaster/Ticketmaster.js
+++ src/components/ticketmaster/Ticketmaster.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Ticketmaster = (props) => {
-  return <div>Hello from Ticketmaster</div>;
+  return (
+    <div>
+      Hello from Ticketmaster <h1>Jamie</h1>
+    </div>
+  );
 };
 
 export default Ticketmaster;
